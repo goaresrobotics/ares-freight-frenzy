a277a6631c4092317053a12679bfee76fb53eca1
Cadence <Cadencethecarder@gmail.com>
2021-12-10 09:59:08 -0800

    Changed power from half (0.5) to 0.6

--- src/com/goaresrobotics/freightfrenzy/teleOpMode.java
+++ src/com/goaresrobotics/freightfrenzy/teleOpMode.java
@@ -47,7 +47,7 @@ public class teleOpMode extends OpMode {
             // Run wheels in tank mode (note: The joystick goes negative when pushed forwards, so negate it)
             left = -gamepad1.left_stick_y;
             right = -gamepad1.right_stick_y;
-            modifier = 0.5;
+            modifier = 0.6;
 
             leftFrontMotor.setPower(left * modifier);
             rightFrontMotor.setPower(right * modifier);
