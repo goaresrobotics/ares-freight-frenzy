f340cd6e474d25d912bb2e41615014ca289652a7
Luca Ricciardi <lucalegomaker@gmail.com>
2022-01-25 17:46:59 -0800

    added n.w. intake motor

--- src/com/goaresrobotics/freightfrenzy/teleOpMode.java
+++ src/com/goaresrobotics/freightfrenzy/teleOpMode.java
@@ -14,6 +14,8 @@ public class teleOpMode extends OpMode {
     DcMotor leftBackMotor;
     DcMotor rightBackMotor;
     DcMotor duckTurnMotor;
+    DcMotor intakeMotor;
+
 
         /*
          * Code to run ONCE when the driver hits INIT
@@ -34,6 +36,8 @@ public class teleOpMode extends OpMode {
             rightBackMotor.setDirection(DcMotorSimple.Direction.REVERSE);
             duckTurnMotor = hardwareMap.get(DcMotor.class, "duckTurnMotor");
             duckTurnMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+            intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
+
 
             // Send telemetry message to signify robot waiting;
             telemetry.addData("Say", "Hello Driver");    //
@@ -48,18 +52,22 @@ public class teleOpMode extends OpMode {
             float rTrigger;
             float lTrigger;
             double modifier;
+            double intake;
 
             // Run wheels in tank mode (note: The joystick goes negative when pushed forwards, so negate it)
             left = gamepad1.left_stick_y;
             right = gamepad1.right_stick_y;
             rTrigger = gamepad1.right_trigger;
             lTrigger = gamepad1.left_trigger;
+            intake = gamepad2.left_stick_y;
             modifier = 0.6;
 
             leftFrontMotor.setPower(left * modifier);
             rightFrontMotor.setPower(right * modifier);
             leftBackMotor.setPower(left * modifier);
             rightBackMotor.setPower(right * modifier);
+            intakeMotor.setPower(intake);
+
             if (lTrigger > 0) {
                 duckTurnMotor.setDirection(DcMotorSimple.Direction.REVERSE);
                 duckTurnMotor.setPower(lTrigger);
